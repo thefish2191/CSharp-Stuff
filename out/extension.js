f2a19500fea50945166f7f94cd64083744627417
The Fish <thefish2191@gmail.com>
2023-02-16 19:44:08 -0800

    Working namespace generator

--- out/extension.js
+++ out/extension.js
@@ -1,8 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
+const vscode = require("vscode");
+const fs = require("fs");
+const join = require("path");
+const dirsRegex = /[\/\\]/g;
 function activate(context) {
-    context.subscriptions.push();
+    function namespaceGenerator(target) {
+        // Get the whole path, with system specific separator, 
+        var rootFolderUri = vscode.workspace.getWorkspaceFolder(target)?.uri.fsPath;
+        // Get the location, folder or file, from where the command was executed, with system specific separator
+        var fullFolderName = target.fsPath;
+        // get the root folder name
+        var rootFolderName = vscode.workspace.getWorkspaceFolder(target).name;
+        // get the folder name where the command was executed, deleting the root folder part, it's empty if was in the root folder
+        var localPath = fullFolderName.replace(rootFolderUri, '');
+        // Creates a possible namespace
+        var newNamespace = localPath.replace(dirsRegex, '.');
+        newNamespace = rootFolderName + newNamespace;
+        return newNamespace;
+    }
+    function findFileLocalPath(target) {
+        // Get the whole path, with system specific separator, 
+        var rootFolderUri = vscode.workspace.getWorkspaceFolder(target)?.uri.fsPath;
+        // Get the location, folder or file, from where the command was executed, with system specific separator
+        var fullFolderName = target.fsPath;
+        // get the root folder name
+        var rootFolderName = vscode.workspace.getWorkspaceFolder(target).name;
+        // get the folder name where the command was executed, deleting the root folder part, it's empty if was in the root folder
+        var fileName = fullFolderName.replace(rootFolderUri, '');
+        // Creates a possible namespace
+        return fileName;
+    }
+    let getNamespace = vscode.commands.registerCommand(`dotnet-helper.namespaceGenerate`, (prayer) => {
+        var newFileName = "test.cs";
+        var newFileFullPath = join.join(prayer.fsPath, newFileName);
+        fs.writeFileSync(newFileFullPath, `namespace ${namespaceGenerator(prayer)};`);
+    });
+    let getFileLocalPath = vscode.commands.registerCommand(`dotnet-helper.fileLocationSpotter`, (prayer) => {
+    });
 }
 exports.activate = activate;
 // This method is called when the extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,55 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as join from 'path';
+
+const dirsRegex = /[\/\\]/g;
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push();
+	function namespaceGenerator(target: any) {
+		// Get the whole path, with system specific separator, 
+		var rootFolderUri = vscode.workspace.getWorkspaceFolder(target)?.uri.fsPath!;
+
+
+		// Get the location, folder or file, from where the command was executed, with system specific separator
+		var fullFolderName = target.fsPath;
+
+
+		// get the root folder name
+		var rootFolderName = vscode.workspace.getWorkspaceFolder(target)!.name;
+
+		// get the folder name where the command was executed, deleting the root folder part, it's empty if was in the root folder
+		var localPath = fullFolderName.replace(rootFolderUri, '');
+
+		// Creates a possible namespace
+		var newNamespace = localPath.replace(dirsRegex, '.');
+		newNamespace = rootFolderName + newNamespace;
+		return newNamespace;
+	}
+	function findFileLocalPath(target: any) {
+		// Get the whole path, with system specific separator, 
+		var rootFolderUri = vscode.workspace.getWorkspaceFolder(target)?.uri.fsPath!;
+
+		// Get the location, folder or file, from where the command was executed, with system specific separator
+		var fullFolderName = target.fsPath;
+
+
+		// get the root folder name
+		var rootFolderName = vscode.workspace.getWorkspaceFolder(target)!.name;
+
+		// get the folder name where the command was executed, deleting the root folder part, it's empty if was in the root folder
+		var fileName = fullFolderName.replace(rootFolderUri, '');
+
+		// Creates a possible namespace
+		return fileName;
+	}
+
+	let getNamespace = vscode.commands.registerCommand(`dotnet-helper.namespaceGenerate`, (prayer) => {
+		var newFileName = "test.cs";
+		var newFileFullPath = join.join(prayer.fsPath, newFileName);
+		fs.writeFileSync(newFileFullPath, `namespace ${namespaceGenerator(prayer)};`);
+	});
+	let getFileLocalPath = vscode.commands.registerCommand(`dotnet-helper.fileLocationSpotter`, (prayer) => {
+	});
 }
 
 // This method is called when the extension is deactivated
